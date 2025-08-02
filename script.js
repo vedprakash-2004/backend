d756d4b438f9919fc9878d277c12d79562638bd6
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-08-03 00:52:41 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// let n = 5;
+
+// for(let i=0; i<n; i++){
+//    console.log("hello,", i);
+// }
+// console.log("bye!");
+
+console.log(process.argv);
+
+let args = process.argv;
+
+
+for(let i=2; i < args.length; i++){
+   console.log("hello to", args[i]);
+}
